890622a16b86d33998bd8d58476b2cac8ae8b212
hackerbotsupreme <ALOKE.GEEK@gmail.com>
2023-10-28 09:48:37 +0530

    Update repository

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
   return (
     <>
     <Router>
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+    <Navbar title="TextUtils(built by Aloke Pramanik)" mode={mode} toggleMode={toggleMode} key={new Date()} />
     <Alert alert={alert}/>
     <div className="container my-3">
     <Switch>
